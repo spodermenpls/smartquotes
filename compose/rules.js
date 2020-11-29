262f7bee946cd391d93b9e3411b1ae657a53bd7a
kkapsner <smartquotes@mail.kkapsner.de>
2020-11-29 23:53:03 +0100

    Added German apostroph
    For #4

--- compose/rules.js
+++ compose/rules.js
@@ -60,7 +60,13 @@ const quoteRules = function(){
 						return "\u201A";
 					}
 					const next = context.next;
+					if (next && previous && !previous.match(noWordEndRegExp) && !next.match(noWordStartRegExp)){
+						return "\u2019";
+					}
 					if (!next || next.match(noWordStartRegExp)){
+						if (previous && !previous.match(/\u201A[^\u2018]*$/)){
+							return "\u2019";
+						}
 						return "\u2018";
 					}
 				}
